125314e9ca6ccf35590767ac3e48f6493d84b71d
claud <claudkubuy84@yahoo.com>
2020-12-06 15:26:05 +0200

    initial commit2

--- connect.php
+++ connect.php
@@ -1,23 +1,17 @@
 <?php
 	$firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    /* 
-    $gender = $_POST['gender'];
-    */	
+    $numbers = $_POST['numbers'];
 	$email = $_POST['email'];
-	$password = $_POST['password'];
-	$number = $_POST['number'];
+	$mesaages = $_POST['mesaages'];
 
 	// Database connection
-	$conn = new mysqli('localhost','root','','test');
+	$conn = new mysqli('localhost','root','','db_kivuem');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		// $stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
-        $stmt = $conn->prepare("insert into registration(firstName, lastName, email, password, number) values(?, ?, ?, ?, ?)");
-        //$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
-        $stmt->bind_param("sssssi", $firstName, $lastName,$email, $password, $number);
+		$stmt = $conn->prepare("insert into feedBack(firstName, numbers, email, mesaages) values(?, ?, ?, ?)");
+        $stmt->bind_param("sssssi", $firstName, $numbers, $email, $mesaages);
 		$execval = $stmt->execute();
 		echo $execval;
 		echo "Envoie successfully...";
